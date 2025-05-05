678d9e0fabc115b65064bdc44d5c007b22013da3
cypherslopps <josephibok36@gmail.com>
2025-05-05 02:58:37 +0100

    Finally fixed svgr webpack issue

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,12 @@ const nextConfig: NextConfig = {
   webpack(config) {
     const fileLoaderRule = config.module.rules.find((rule: any) => rule.test?.test?.('.svg'));
     
-    fileLoaderRule.exclude = /\.svg$/i;
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
     
     config.module.rules.push({
       test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
       use: ["@svgr/webpack"]
     })
 
